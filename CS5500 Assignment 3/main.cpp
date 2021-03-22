bffe40a1b7e0540511e414bfaefb30ac31dc0db9
Noah-Huff <noah.b.huff@gmail.com>
2021-03-22 11:13:37 -0500

    testing GitHub connection

--- CS5500 Assignment 3/main.cpp
+++ CS5500 Assignment 3/main.cpp
@@ -3,12 +3,37 @@
 //  CS5500 Assignment 3
 //
 //  Created by Noah Huff on 3/22/21.
-//
+//  ID# 700715656
+//  “I certify that the code in the method functions including method function
+//  main of this project are entirely my own work.”
 
 #include <iostream>
 
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/errno.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+#include <unistd.h>
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // create semaphores
+    
+    // set semaphore initial values
+    
+    // create shared memory segment
+    key_t shm_key;
+    int shm_flag;
+    int shm_id;
+    int shm_size;
+    
+    
+    // child process
+    fork();
+    
+    
+    
     return 0;
 }
