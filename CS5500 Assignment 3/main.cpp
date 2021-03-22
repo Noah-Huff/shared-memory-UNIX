fb330accc2d4d903c3b19a18d9909745511ec062
Noah-Huff <noah.b.huff@gmail.com>
2021-03-22 07:40:24 -0500

    Initial Commit

---
+++ CS5500 Assignment 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS5500 Assignment 3
+//
+//  Created by Noah Huff on 3/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
