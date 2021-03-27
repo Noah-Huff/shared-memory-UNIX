c13a4767a03a9e32ba81701a95dec41f4920d01c
Noah-Huff <noah.b.huff@gmail.com>
2021-03-27 09:57:44 -0500

    I was having a lot of issues, so I built this in another project and copied it here when it was working.

--- CS5500 Assignment 3/main.cpp
+++ CS5500 Assignment 3/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  CS5500 Assignment 3
+//  CS5500 Assignment 3 
 //
 //  Created by Noah Huff
 //  UCM ID# 700715656
@@ -8,195 +8,118 @@
 //  main of this project are entirely my own work.”
 
 #include <iostream>
-
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/errno.h>
+#include <semaphore.h>
 #include <stdio.h>
-#include <sys/shm.h>
-#include <stdlib.h>
-#include <sys/sem.h>
 #include <unistd.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 
-#define SEM_PERM 0600
 #define SHM_PERM 0600
-void check_sem_error(int ret_value);
-void print_sem_value(int id, int semaphore);
 
 int main(int argc, const char * argv[]) {
-    srand((int)time(NULL));
-    // create semaphores
-    int sem_id = semget(IPC_PRIVATE, 3, SEM_PERM | IPC_CREAT | IPC_EXCL);
-    // i created three semaphores, one for full, one for empty, and the last
-    // one for the mutex. Here, I assign names to the three semaphores
-    // to make using them less confusing
-    const int sem_full = 0;
-    const int sem_empty = 1;
-    const int sem_mutex = 2;
-    
-    // set semaphore initial values
-    union semun all_sem;
-    int ret_value;
-    int full = 0;
-    int empty = 10;
-    const int mutex_initial = 1;
-    
-    all_sem.val = full;
-    ret_value = semctl(sem_id, sem_full, SETVAL, all_sem.val);
-    check_sem_error(ret_value);
-    all_sem.val = empty;
-    ret_value = semctl(sem_id, sem_empty, SETVAL, all_sem.val);
-    check_sem_error(ret_value);
-    all_sem.val = mutex_initial;
-    ret_value = semctl(sem_id, sem_mutex, SETVAL, all_sem.val);
-    check_sem_error(ret_value);
-    // setting the structs for each semaphore operation
-    // full semaphore actions
-    struct sembuf sop_full_inc = { sem_full, +1, SEM_UNDO};
-    struct sembuf sop_full_dec = { sem_full, -1, SEM_UNDO};
-    // empty semaphore actions
-    struct sembuf sop_empty_inc = { sem_empty, +1, SEM_UNDO};
-    struct sembuf sop_empty_dec = { sem_empty, -1, SEM_UNDO};
-    // mutex semaphore actions
-    struct sembuf sop_mutex_up = { sem_mutex, +1, SEM_UNDO};
-    struct sembuf sop_mutex_down = { sem_mutex, -1, SEM_UNDO};
-
-    printf("SEM_FULL %d\n", semctl(sem_id, sem_full, GETVAL, 0));
-    printf("SEM_EMPTY %d\n", semctl(sem_id, sem_empty, GETVAL, 0));
-    printf("SEM_MUTEX %d\n", semctl(sem_id, sem_mutex, GETVAL, 0));
-    printf("SEM_ID %d\n", sem_id);
-
-    // create shared memory segment
-    size_t shm_size = 10;
+    //shared memory
+    //I added one more slot to hold an index
+    size_t shm_size = 11;
     int shm_id = shmget(IPC_PRIVATE, shm_size, SHM_PERM | IPC_CREAT | IPC_EXCL);
-    
     printf("SHM_ID %d\n", shm_id);
-    
-    // using fork() to create a child process
-    pid_t parent_process = getpid();
-    pid_t proc_id = fork();
-    pid_t child_process;
-    if ( proc_id < 0 ) {
-        fprintf(stderr, "Child Process Creation Failed");
-        return 1;
-    } else if ( proc_id == 0 ) {
-        // ----------------- child process -------------------
-        char * child_buf;
-        child_buf = (char*)shmat(shm_id, (char*)0, 0);
-        child_process = getpid();
-        printf("Child Process PID %d\n", child_process);
-        /*
-        int i = 0;
-        while ( i < 10 ) {
-            printf("C_INT: %d\n", *child_buf);
-            child_buf+=4; // imcrement 4, the size of an integer
-            i++;
+    // semaphores with their initial values
+    sem_t *mutex = sem_open("mutex", O_CREAT | O_EXCL, 0, 1);
+    sem_t *full = sem_open("full", O_CREAT | O_EXCL, 0, 0);
+    sem_t *empty = sem_open("empty", O_CREAT | O_EXCL, 0, 10);
+    sem_unlink("empty");
+    sem_unlink("full");
+    sem_unlink("mutex");
+    srand(time_t(NULL));
+    int consumer = fork();
+    int parent = getpid();
+    int child;
+    if ( consumer < 0 ) {
+        printf("CONSUMER FORK FAILED.");
+    } else if ( consumer == 0 ) {
+        child = getpid();
+        // --------------CONSUMER---------------
+        sem_wait(mutex);
+        printf("CONSUMER PID: %d\n", child);
+        sem_post(mutex);
+        int * cons_buf;
+        cons_buf = (int*)shmat(shm_id, (int*)0, 0);
+        int * start_point = cons_buf;
+        int total = 0;
+        int offset = 0;
+        for ( int i = 0; i < 1000; i++ ) {
+            int sleep_time = rand() % 3;
+            if ( i % 3 == sleep_time * 10) {
+                printf("$$$$$$$$$$Consumer Sleeping$$$$$$$$$$\n");
+                usleep(sleep_time);
+            }
+            sem_wait(full);
+            sem_wait(mutex);
+            printf("---Consumer Critical Section---\n");
+            /*------------------consume items------------------*/
+            // go to the first element to get the offset
+            cons_buf = start_point;
+            offset = *cons_buf;
+            printf("Consumer Offset: %d\n", *cons_buf);
+            //decrement the offset
+            *cons_buf = offset - 1;
+            //go to the correct memory slot
+            cons_buf = (start_point + offset);
+            printf("Consumer Buffer Position: %p Value: %d\n", cons_buf, *cons_buf);
+            //add item to total
+            total += *cons_buf;
+            printf("Current Total: %d\n", total);
+            /*-------------end of comsuming items--------------*/
+            printf("---Exiting Critical Section---\n");
+            sem_post(mutex);
+            sem_post(empty);
         }
-         */
-        int ret_value = -1;
-        for ( int i = 0; i < 10; i++ ) {
-        int start_consuming_here = semctl(sem_id, sem_full, GETVAL, 0);
-        if ( semctl(sem_id, sem_mutex, GETVAL, 0) ) {
-            semop( sem_id, &sop_mutex_down, 1 );
-            // consumer starts at the end of the full buffer
-             // decrement the full buffer
-            printf("\n\nMADE IT TO CHILD %d\n\n", semctl(sem_id, sem_mutex, GETVAL, 0));
-            check_sem_error(semop( sem_id, &sop_empty_inc, 0 ));
-            //printf("\n\n2_MADE IT TO CHILD\n\n");
-            check_sem_error(semop( sem_id, &sop_full_dec, 0 ));
-            //printf("\n\n3_MADE IT TO CHILD %d\n\n", semctl(sem_id, sem_mutex, GETVAL, 0));
-            semop( sem_id, &sop_mutex_up, 1 );
-            printf("\n\nLEAVING CHILD %d\n\n", semctl(sem_id, sem_mutex, GETVAL, 0));
-
-        }
-     }
         exit(0);
-    } else {
-        // ---------------- parent process -------------------
-        /*
-        semop(sem_id, &sop_full_inc, 1);
-        printf("SOP_FULL_INC %d\n", semctl(sem_id, sem_full, GETVAL, 0));
-        semop(sem_id, &sop_full_dec, 1);
-        printf("SOP_FULL_DEC %d\n", semctl(sem_id, sem_full, GETVAL, 0));
-         */
-        int * parent_buf;
-        parent_buf = (int*)shmat(shm_id, (int*)0, 0);
-        printf("Parent Process PID %d\n", parent_process);
-        int buffer_number = 1;
-        /*
-        int j = 0;
-        while ( j < 10 ) {
-            *parent_buf = j;
-            printf("P_INT: %d\n", *parent_buf);
-            parent_buf++;
-            j++;
+    } else if ( getpid() == parent ) {
+        // -------------PRODUCER----------------
+        //sem_post increments
+        //sem_wait decrements
+        printf("PRODUCER PID: %d\n", parent);
+        int * prod_buf;
+        prod_buf = (int*)shmat(shm_id, (int*)0, 0);
+        int * start_point = prod_buf;
+        int offset = 0;
+        int item_produced = 1;
+        for( int i = 0; i < 1000; i++ ) {
+            int sleep_time = rand() % 3;
+            if ( i % 2 == sleep_time * 700) {
+                printf("**********Producer Sleeping**********\n");
+                usleep(sleep_time);
+            }
+            sem_wait(empty);
+            sem_wait(mutex);
+            printf("+++Producer Critical Section+++\n");
+            /*---------------produce items----------------*/
+            // go to the first element to get the offset
+            prod_buf = start_point;
+            offset = *prod_buf;
+            //increment the offset
+            *prod_buf = offset + 1;
+            printf("----------------------------Number Of Items In The Buffer: %d\n", *prod_buf);
+            //go to the correct memory slot
+            prod_buf = start_point + *prod_buf;
+            //add item_produced in the correct location
+            *prod_buf = item_produced;
+            printf("Producer Buffer Position: %p Value: %d\n", prod_buf, *prod_buf);
+            //increment the count for the next item
+            item_produced++;
+            /*-----------end of producing items-------------*/
+            printf("+++Exiting Critical Section+++\n");
+            sem_post(mutex);
+            sem_post(full);
         }
-         */
-        for ( int i = 0; i < 10; i++) {
-          int start_filling_here = semctl(sem_id, sem_full, GETVAL, 0);
-          if ( semctl(sem_id, sem_mutex, GETVAL, 0) && start_filling_here < 10 ) {
-              semop( sem_id, &sop_mutex_down, 1 );
-              printf("\n\nMADE IT TO PARENT %d\n\n", semctl(sem_id, sem_mutex, GETVAL, 0));
-              //*parent_buf = start_filling_here;
-              //printf("P_INT: %d\n", parent_buf);
-              //*parent_buf = buffer_number;
-              //printf("P_INT: %d\n", *parent_buf);
-              printf("P_INT: %d\n", parent_buf);
-              //semop( sem_id, &sop_empty_dec, 0 );
-              semop( sem_id, &sop_full_inc, 0 );
-              printf("\nSTART_FILLING_HERE %d\n", start_filling_here);
-              //buffer_number++;
-              semop( sem_id, &sop_mutex_up, 1 );
-              printf("\nLEAVING PARENT %d\n", semctl(sem_id, sem_mutex, GETVAL, 0));
-              
-              int sleep_time = (rand() % 8) * 10;
-              printf("Producer sleep time %d\n", sleep_time);
-            
-              usleep(sleep_time);
-          }
-
-      }
     }
-    /*
-    if ( child_process == getpid() ) {
-        printf("CHILD_____PROCESS");
-    } else if ( parent_process == getpid() ) {
-        printf("PARENT_______PROCESS");
+    if(consumer > 0)
+    {
+        int status;
+        printf("\nProducer Finished, Waiting For The Consumer\n\n");
+        wait(&status);
     }
-     */
-    
-    // wait for child to destroy the semaphores and shared memory
-    parent_process = wait(&child_process);
-    semctl(sem_id, sem_full, IPC_RMID, 0);
-    semctl(sem_id, sem_empty, IPC_RMID, 0);
-    semctl(sem_id, sem_mutex, IPC_RMID, 0);
-    
+    parent = wait(&child);
     shmctl(shm_id, IPC_RMID, NULL);
     return 0;
-}
-
-void parent_producer() {
-    int sleep_time = (rand() % 8) * 5;
-    printf("Producer sleep time %d\n", sleep_time);
-    
-    usleep(sleep_time);
-}
-
-void child_consumer() {
-    
-}
-
-// print out the value of the semaphore
-void print_sem_value(int id, int semaphore) {
-    printf("Semaphore value %d\n", semctl(id, semaphore, GETVAL, 0));
-}
-
-// check for semaphore errors
-void check_sem_error(int ret_value) {
-    if ( ret_value == -1 ) {
-        perror("CHECK_SEM_ERROR");
-        int err = errno;
-        printf("Error #: %d\n", err);
-        exit(1);
-    }
 }
